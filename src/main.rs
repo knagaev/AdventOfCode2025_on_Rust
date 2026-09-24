9b39f85cbf83f87f4736ada3436be8b6d20de75b
Konstantin Nagaev <nagaev.kv@phystech.edu>
2026-09-24 15:07:57 +0300

    Day 2 1 done

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-#[path = "task_01.rs"]
-mod task_01;
-
+//mod task_01;
 mod task_02;
 
-fn main() {
-    task_02::local_main();
+use std::io;
+
+fn main() -> io::Result<()> {
+    task_02::local_main()
 }
