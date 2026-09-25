25bccde0361a02f8551cedb27815d2e9b9fc7f4f
Konstantin Nagaev <nagaev.kv@phystech.edu>
2026-09-25 13:46:07 +0300

    Day 3 2 done + canonical

--- src/task_03.rs
+++ src/task_03.rs
@@ -8,6 +8,7 @@ pub fn local_main() -> io::Result<()> {
 
     let mut total_max_joltage = 0_u32;
     let mut total_max_joltage_12 = 0_u64;
+    let mut total_max_joltage_u128 = 0_u128;
 
     for bank in reader.lines() {
         let bank = bank?; // line здесь имеет тип Result<String>, распаковываем его
@@ -25,10 +26,18 @@ pub fn local_main() -> io::Result<()> {
                 "get_max_joltage_12 не вернула результат!",
             )
         })?;
+
+        total_max_joltage_u128 += get_max_joltage_u128(&bank, 12).ok_or_else(|| {
+            io::Error::new(
+                io::ErrorKind::InvalidData,
+                "get_max_joltage_12 не вернула результат!",
+            )
+        })?;
     }
 
     println!("total_max_joltage {:?}", total_max_joltage);
     println!("total_max_joltage_12 {:?}", total_max_joltage_12);
+    println!("total_max_joltage_u128 {:?}", total_max_joltage_u128);
     Ok(())
 }
 
@@ -77,4 +86,39 @@ fn get_max_joltage_12(bank: &str) -> Option<u64> {
 
     //println!("max_joltage {:?}", max_joltage);
     Some(max_joltage)
+}
+
+/// каноническая версия поиска максимальной подпоследовательности со стеком
+fn get_max_joltage_u128(s: &str, k: usize) -> Option<u128> {
+    println!("{}", s);
+    let n = s.len(); // Для ASCII-цифр len() == кол-ву символов
+    if k > n || k == 0 {
+        return None;
+    }
+
+    let to_remove = n - k;
+
+    let mut stack: Vec<u8> = Vec::with_capacity(k);
+    let mut removed = 0;
+
+    for &b in s.as_bytes() {
+        while removed < to_remove && !stack.is_empty() && *stack.last().unwrap() < b {
+            stack.pop();
+            removed += 1;
+            //println!("stack {:?}", String::from_utf8_lossy(&stack));
+        }
+        stack.push(b);
+        //println!("stack {:?}", String::from_utf8_lossy(&stack));
+    }
+
+    // обрезаем до нужной длины
+    stack.truncate(k);
+
+    // преобразование в число
+    let mut result: u128 = 0;
+    for &b in &stack {
+        result = result * 10 + (b - b'0') as u128;
+    }
+
+    Some(result)
 }
